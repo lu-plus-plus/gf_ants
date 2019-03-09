285794007545bdac76612980a70ba4f93b445ef0
LUplusplus <lujialiang.whu.edu.cn>
2019-03-09 20:29:29 +0800

    Rewrite finished. Now going to test it.

--- gf_int.h
+++ gf_int.h
@@ -1,156 +1,186 @@
-#include "xint.h"
+#include <cstdint>
+#include <type_traits>
+
+template <bool IF>
+using bool_constant = std::integral_constant<bool, IF>;
+
+constexpr bool in_closed_range(const int val, const int l, const int r)
+{
+	return l <= val && val <= r;
+}
+
+template <int BITS, typename = bool_constant<true>>
+struct gf_raw;
+
+template <int BITS>
+struct gf_raw<BITS, bool_constant<in_closed_range(BITS, 1, 8)>> {
+	using type = uint8_t;
+};
+
+template <int BITS>
+struct gf_raw<BITS, bool_constant<in_closed_range(BITS, 9, 16)>> {
+	using type = uint16_t;
+};
+
+template <int BITS>
+struct gf_raw<BITS, bool_constant<in_closed_range(BITS, 17, 32)>> {
+	using type = uint32_t;
+};
+
+template <int BITS>
+struct gf_raw<BITS, bool_constant<in_closed_range(BITS, 33, 64)>> {
+	using type = uint64_t;
+};
 
 
 
-// ****************************************
-// class gf_int
-// ****************************************
-
-// Declaration of gf_int and its friend functions
 
 template <int BITS>
 class gf_int;
 
-// special optimization where n = 2^k
+
 
 constexpr bool is_power_of_2(int n) {
 	return (n == 1) ? true : ( (n%2 == 0) ? is_power_of_2(n/2) : false );
 }
 
 template <int BITS, typename = bool_constant<true>>
-struct reduce_underlying;
+struct reduction;
 
 template <int BITS>
-struct reduce_underlying< BITS, bool_constant<is_power_of_2(BITS)> > {
-	__device__ inline static bool index_check(const int digit, const int partner, const int stride) {
+struct reduction< BITS, bool_constant<is_power_of_2(BITS)> > {
+	__host__ __device__ inline static bool index_check(const int digit, const int partner, const int stride) {
 		return digit % stride == 0;
 	}
 };
 
 template <int BITS>
-struct reduce_underlying< BITS, bool_constant<is_power_of_2(BITS) == false> > {
-	__device__ inline static bool index_check(const int digit, const int partner, const int stride) {
+struct reduction< BITS, bool_constant<is_power_of_2(BITS) == false> > {
+	__host__ __device__ inline static bool index_check(const int digit, const int partner, const int stride) {
 		return digit % stride == 0 && partner < BITS;
 	}
 };
 
-// Different functions for calculating
-// Most and Least half of carry-less multiplication
 
-template <int BITS, typename T>
-__device__ inline T a_mul_bi_least_half(T a, T b, int digit) {
+
+template <int BITS>
+__host__ __device__ inline constexpr
+typename gf_raw<BITS>::type mask(const typename gf_raw<BITS>::type memory) {
+	using T = typename gf_raw<BITS>::type;
+	constexpr int raw_len = sizeof(T) * 8;
+    constexpr T mask_bits = static_cast<T>(-1) >> (raw_len - BITS);
+	return memory & mask_bits;
+}
+
+template <int BITS>
+__host__ __device__ inline constexpr
+typename gf_raw<BITS>::type right_shift_in(const typename gf_raw<BITS>::type memory, const int offset) {
+    return mask<BITS>(memory) >> offset;
+}
+
+
+
+template <int BITS>
+__host__ __device__ inline typename gf_raw<BITS>::type split_least_result
+	(const typename gf_raw<BITS>::type a, const typename gf_raw<BITS>::type b, const int digit) {
+	using T = typename gf_raw<BITS>::type;
 	return a * (b & (static_cast<T>(1) << digit));
 }
 
-template <int BITS, typename T>
-__device__ inline T a_mul_bi_most_half(T a, T b, int digit) {
-	return (a >> (BITS - digit)) * ((b >> digit) & static_cast<T>(1));
+template <int BITS>
+__host__ __device__ inline typename gf_raw<BITS>::type split_most_result
+	(typename gf_raw<BITS>::type a, typename gf_raw<BITS>::type b, int digit) {
+	using T = typename gf_raw<BITS>::type;
+	return right_shift_in<BITS>(a, BITS-digit) * (right_shift_in<BITS>(b, digit) & static_cast<T>(1));
 }
 
 
-
-// Declaration: Irreducible Polynomials
 
 template <int BITS>
 struct constepxr_irreducible;
 
-// Defination: Irreducible Polynomials
-
 template <>
 struct constepxr_irreducible<4> {
-	static constexpr xint<4>::raw_t value() {
-		return 0x13;
-	}
+	static constexpr typename gf_raw<4 * 2>::type polynomial = 0x13;
 };
 
 
-
-// Defination
 
 template <int BITS>
 class gf_int
 {
 public:
-	using std_t = xint<BITS>;
-	using unsafe_t = typename std_t::raw_t;
-	using ext_t = xint<BITS*2>;
+	using raw_t = typename gf_raw<BITS>::type;
+	using ext_t = typename gf_raw<BITS * 2>::type;
 
 private:
-	std_t data;
+	raw_t memory;
 
 private:
-	static constexpr ext_t polynomial_divide(ext_t poly, int digit = BITS-1) {
-		return ((((static_cast<ext_t>(1) << (BITS+digit)) & poly) != 0) ? (static_cast<ext_t>(1)<<digit) : 0) |
-			polynomial_divide(poly ^ (irred_g * ((poly >> BITS) & (static_cast<ext_t>(1) << digit))), digit-1);
-
-		/*constexpr bool flag = ((static_cast<ext_t>(1) << (BITS+digit)) & poly) != 0;
-		poly = flag ? (poly ^ (irred_g << digit)) : poly;
-		return (digit < BITS) ? 0 : (
-			(flag ? (static_cast<ext_t>(1)<<digit) : 0) | polynomial_divide(poly, digit-1)
-		);*/
-	}
-public:
-	static constexpr ext_t irred_g = constepxr_irreducible<BITS>::value();
-	static constexpr std_t g_star = static_cast<std_t>(irred_g);
-	static constexpr ext_t q_plus = (static_cast<ext_t>(1) << BITS) |
-		polynomial_divide(irred_g << BITS);
-
-	__device__ unsafe_t test() const {
-		return irred_g.value();
+	static constexpr ext_t polynomial_divide(const ext_t poly, const int digit) {
+		const ext_t bit_pick = right_shift_in<BITS*2>(poly, BITS) & (static_cast<ext_t>(1) << digit);
+		const ext_t addition = irred_g * bit_pick;
+		return ( bit_pick ? (static_cast<ext_t>(1)<<digit) : 0 ) |
+			( (digit > 0) ? polynomial_divide(poly^addition, digit-1) : 0 );
 	}
 
 public:
-	__host__ __device__ constexpr gf_int(unsafe_t unsafe_data): data(unsafe_data) {}
-	__host__ __device__ constexpr gf_int(const gf_int &old): data(old.data) {}
+	static constexpr ext_t irred_g = constepxr_irreducible<BITS>::polynomial;
+	static constexpr raw_t g_star = static_cast<raw_t>( mask<BITS>(irred_g) );
+	static constexpr ext_t q_plus = (static_cast<ext_t>(1) << BITS)
+		| polynomial_divide(mask<BITS>(irred_g) << BITS, BITS - 1);
+		// Mask<>() to avoid integer overflow warning
 
-	__device__ constexpr unsafe_t value() const {
-		return data.value();
+public:
+	__host__ __device__ constexpr gf_int(raw_t raw_memory): memory(raw_memory) {}
+	__host__ __device__ constexpr gf_int(const gf_int &old): memory(old.memory) {}
+
+	__host__ __device__ constexpr raw_t value() const {
+		return mask<BITS>(memory);
 	}
 
-	__device__ constexpr gf_int & operator+=(const gf_int &rhs) {
-		data ^= rhs.data;
+	__host__ __device__ constexpr gf_int & operator+=(const gf_int &rhs) {
+		memory ^= rhs.memory;
 		return *this;
 	}
 
-	// carry-less multiplication
-	// Warning: Better Reduction Algorithm?
-	
-	template <std_t (*f)(std_t a, std_t b, int digit)>
+	template <raw_t (*f)(raw_t a, raw_t b, int digit)>
 	__device__ gf_int & clmuled_by(const gf_int &rhs) {
 		gf_int &lhs = *this;
 	
 		int digit = threadIdx.z;
-		__shared__ unsafe_t c[BITS];
+		__shared__ raw_t c[BITS];
 
-		c[digit] = f(lhs.data, rhs.data, digit).value();
+		c[digit] = f(lhs.memory, rhs.memory, digit);
 		__syncthreads();
 
+		// Warning: Better Reduction Algorithm?
 		for (int stride = 2; stride <= BITS; stride *= 2) {
 			int partner = digit + (stride >> 1);
-			if (reduce_underlying<BITS>::index_check(digit, partner, stride)) {
+			if (reduction<BITS>::index_check(digit, partner, stride)) {
 				c[digit] ^= c[partner];
 			}
 			__syncthreads();
 		}
 
-		lhs.data = c[0];
+		lhs.memory = c[0];
 		return lhs;
 	}
 
 	__device__ gf_int & clmuled_least_by(const gf_int &rhs) {
-		return clmuled_by<a_mul_bi_least_half<BITS, std_t>>(rhs);
+		return clmuled_by<split_least_result<BITS>>(rhs);
 	}
 
 	__device__ gf_int & clmuled_most_by(const gf_int &rhs) {
-		return clmuled_by<a_mul_bi_most_half<BITS, std_t>>(rhs);
+		return clmuled_by<split_most_result<BITS>>(rhs);
 	}
 
 	// Galois Field Multiplication
-	// ... ...
+	__device__ gf_int & gf_muled_by(const gf_int &rhs);
 };
 
 template <int BITS>
-__device__ constexpr gf_int<BITS> operator+(const gf_int<BITS> &lhs, const gf_int<BITS> &rhs)
+__host__ __device__ constexpr gf_int<BITS> operator+(const gf_int<BITS> &lhs, const gf_int<BITS> &rhs)
 {
 	gf_int<BITS> result(lhs);
 	result += rhs;
@@ -173,6 +203,22 @@ __device__ gf_int<BITS> clmul_most(const gf_int<BITS> &lhs, const gf_int<BITS> &
 	return result;
 }
 
+template <int BITS>
+__device__ gf_int<BITS> gf_mul(const gf_int<BITS> &lhs, const gf_int<BITS> &rhs)
+{
+	using raw_t = typename gf_int<BITS>::raw_t;
+	gf_int<BITS> c = clmul_most(lhs, rhs);
+	gf_int<BITS> c_least = clmul_least(lhs, rhs);
+
+	c += clmul_most<BITS>(c, gf_int<BITS>::q_plus);
+	// The most significant bit of q_plus is always 1,
+	// so we add a copy of c all the time.
+
+	c.clmuled_least_by(gf_int<BITS>::g_star);
+
+	return c + c_least;
+}
+
 
 
 // Type Aliases
@@ -180,4 +226,3 @@ __device__ gf_int<BITS> clmul_most(const gf_int<BITS> &lhs, const gf_int<BITS> &
 using gf_int8_t = gf_int<8>;
 using gf_int16_t = gf_int<16>;
 using gf_int32_t = gf_int<32>;
-using gf_int64_t = gf_int<64>;
--- main.cu
+++ main.cu
@@ -15,15 +15,17 @@ __global__ void test(gf_test_t *_a, gf_test_t *_b)
 
 	auto r1 = a+b;
 	auto r2 = clmul_least(a, b);
-	auto r3 = clmul_most(a, b);
+    auto r3 = clmul_most(a, b);
+    auto r4 = gf_mul(a, b);
 
 	if (threadIdx.z == 0) {
 		printf("0x%llx\n", static_cast<uint64_t>(r1.value()));
 		printf("0x%llx\n", static_cast<uint64_t>(r2.value()));
         printf("0x%llx\n", static_cast<uint64_t>(r3.value()));
-        // printf("0x%llx\n", static_cast<uint64_t>(r3.test()));
-        /*printf("0x%llx\n", static_cast<uint64_t>(gf_test_t::g_star.value()));
-        printf("0x%llx\n", static_cast<uint64_t>(gf_test_t::q_plus.value()));*/
+        printf("0x%llx\n", static_cast<uint64_t>(gf_test_t::irred_g));
+        printf("0x%llx\n", static_cast<uint64_t>(gf_test_t::g_star));
+        printf("0x%llx\n", static_cast<uint64_t>(gf_test_t::q_plus));
+        printf("0x%llx\n", static_cast<uint64_t>(r4.value()));
 	}
 
 }
