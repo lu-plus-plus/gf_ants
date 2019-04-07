1fd03c1a8e9aca299ca7836a2e6f43f1422b1961
LUplusplus <lujialiang.whu.edu.cn>
2019-04-07 10:14:34 +0800

    A block-level version for HPC.

--- main.cu
+++ main.cu
@@ -13,7 +13,7 @@ constexpr bool PRINT_RESULT = false;
 
 using gf_int_t = gf_int<APP_BITS>;
 
-constexpr int CAPABLE_DIM = APP_DIM / 2;
+constexpr int CAPABLE_DIM = APP_DIM;
 using square_t = gf_square<gf_int_t, CAPABLE_DIM>;
 
 
@@ -102,6 +102,17 @@ void init_identify(square_t *obj) {
 	}
 }
 
+void init_whole(square_t *obj) {
+	for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
+		for (uint32_t j = 0; j < i; ++j) {
+			obj->data[i][j] = gf_int_t(0);
+		}
+		for (uint32_t j = i; j < CAPABLE_DIM; ++j) {
+			obj->data[i][j] = gf_int_t(i + j + 1);
+		}
+	}
+}
+
 
 
 int main(void)
@@ -110,16 +121,10 @@ int main(void)
 		std::cout << "The size of square_t isn't multiple of block size." << std::endl;
 		throw std::exception();
 	}
-	if (APP_DIM != CAPABLE_DIM * 2) {
-		std::cout << "The size of square matrix must be twice CAPABLE_DIM." << std::endl;
-		throw std::exception();
-	}
 
 	try {
 		cuder<square_t> buf_1(make_cuder<square_t>());
 		cuder<square_t> buf_2(make_cuder<square_t>());
-		cuder<square_t> buf_3(make_cuder<square_t>());
-		cuder<square_t> buf_4(make_cuder<square_t>());
 
 		// Time Counter Initialization
 		cudaEvent_t start, stop;
@@ -130,81 +135,9 @@ int main(void)
 			cudaEventRecord(start, 0);
 		}
 
-		buf_1.load(init_A);
+		buf_1.load(init_whole);
 		buf_2.load(init_identify);
 		square_inverse(buf_1, buf_2);
-		// buf_2 = A_inv
-
-		buf_1.load(init_C);
-		square_mul(buf_1, buf_2, buf_3);
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-
-		buf_1.load(init_B);
-		square_mul(buf_3, buf_1, buf_4);
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-		// buf_4 = C * A_inv * B
-
-		buf_1.load(init_D);
-		square_add(buf_4, buf_1);
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-		// buf_4 = D - C * A_inv * B
-
-		buf_1.load(init_identify);
-		square_inverse(buf_4, buf_1);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-
-		square_mul(buf_1, buf_3, buf_4);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-		// buf_4 = DL
-
-		buf_3.store("C_Ainv");
-		buf_4.store("DL");
-		// buf_1 = DR
-		// buf_2 = A_inv
-		
-		buf_3.load(init_B);
-		square_mul(buf_2, buf_3, buf_4);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_4 = A_inv * B
-
-		square_mul(buf_4, buf_1, buf_3);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = A_inv * B
-
-		buf_1.store("DR");
-		buf_4.load("C_Ainv");
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = C * A_inv
-
-		square_mul(buf_3, buf_4, buf_1);
-		// buf_1 = UR * C * A_inv
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = C * A_inv
-
-		square_add(buf_1, buf_2);
-		// buf_1 = UL
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = C * A_inv
-
-		buf_2.load("DL");
-		buf_4.load("DR");
-		// buf_1 = Up Left
-		// buf_2 = Down Left
-		// buf_3 = Up Right
-		// buf_4 = Down Right
 
 		if (PRINT_TIME) {
 			cudaEventRecord(stop, 0);
@@ -217,20 +150,7 @@ int main(void)
 		if (PRINT_RESULT) {
 			for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
 				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_1->data[i][j] << ' ';
-				}
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_3->data[i][j] << ' ';
-				}
-				std::cout << std::endl;
-			}
-	
-			for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
 					std::cout << std::hex << buf_2->data[i][j] << ' ';
-				}
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_4->data[i][j] << ' ';
 				}
 				std::cout << std::endl;
 			}
