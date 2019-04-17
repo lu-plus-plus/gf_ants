c140cfc2db548e628ffbd7679823a6f3d5330e8f
LUplusplus <lujialiang.whu.edu.cn>
2019-04-17 12:18:25 +0800

    a correct REAL virtual memory version

--- main.cu
+++ main.cu
@@ -6,7 +6,7 @@
 
 #define APP_DIM (2048)
 
-constexpr bool PRINT_TOTAL_TIME = true;
+constexpr bool PRINT_TOTAL_TIME = false;
 constexpr bool PRINT_VERBOSE = false;
 constexpr bool PRINT_RESULT = false;
 
@@ -14,12 +14,12 @@ constexpr bool PRINT_RESULT = false;
 
 using gf_int_t = gf_int<APP_BITS>;
 
-constexpr int CAPABLE_DIM = APP_DIM / 2;
-using square_t = gf_square<gf_int_t, CAPABLE_DIM>;
+constexpr int CAPABLE_DIM = 512;
+using capable_t = gf_square<gf_int_t, CAPABLE_DIM>;
 
 
 
-void square_inverse(square_t *d_A_ptr, square_t *d_B_ptr)
+void capable_inverse(capable_t *d_A_ptr, capable_t *d_B_ptr)
 {
 	dim3 grid(INVERSE_GRID_DIM_X);
 	dim3 block(BLOCK_DIM, BLOCK_DIM);
@@ -35,7 +35,7 @@ void square_inverse(square_t *d_A_ptr, square_t *d_B_ptr)
 	cudaDeviceSynchronize();
 }
 
-void square_mul(square_t *d_A_ptr, square_t *d_B_ptr, square_t *d_C_ptr)
+void capable_mul(capable_t *d_A_ptr, capable_t *d_B_ptr, capable_t *d_C_ptr)
 {
 	dim3 grid(16, 16);
 	dim3 block(BLOCK_DIM, BLOCK_DIM);
@@ -44,7 +44,7 @@ void square_mul(square_t *d_A_ptr, square_t *d_B_ptr, square_t *d_C_ptr)
 	cudaDeviceSynchronize();
 }
 
-void square_add(square_t *d_A_ptr, square_t *d_B_ptr)
+void capable_add(capable_t *d_A_ptr, capable_t *d_B_ptr)
 {
 	dim3 grid(16, 16);
 	dim3 block(BLOCK_DIM, BLOCK_DIM);
@@ -55,188 +55,178 @@ void square_add(square_t *d_A_ptr, square_t *d_B_ptr)
 
 
 
-void init_A(square_t *obj) {
-	for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-		for (uint32_t j = 0; j < i; ++j) {
-			obj->data[i][j] = gf_int_t(0);
-		}
-		for (uint32_t j = i; j < CAPABLE_DIM; ++j) {
-			obj->data[i][j] = gf_int_t(i + j + 1);
-		}
-	}
-}
-
-void init_B(square_t *obj) {
-	for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-		for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-			obj->data[i][j] = gf_int_t(i + (CAPABLE_DIM + j) + 1);
-		}
-	}
-}
-
-void init_C(square_t *obj) {
+void init_identify(capable_t *obj) {
 	for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
 		for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
 			obj->data[i][j] = gf_int_t(0);
 		}
-	}
-}
-
-void init_D(square_t *obj) {
-	for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-		for (uint32_t j = 0; j < i; ++j) {
-			obj->data[i][j] = gf_int_t(0);
-		}
-		for (uint32_t j = i; j < CAPABLE_DIM; ++j) {
-			obj->data[i][j] = gf_int_t(2*CAPABLE_DIM + i + j + 1);
-		}
-	}
-}
-
-void init_identify(square_t *obj) {
-	for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-		for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-			obj->data[i][j] = gf_int_t(0);
-		}
-		
 		obj->data[i][i] = gf_int_t(1);
 	}
 }
 
 
 
+inline std::string piece(int i, int j) {
+	return std::to_string(i) + "_" + std::to_string(j);
+}
+
 int main(void)
 {
 	if (CAPABLE_DIM % BLOCK_DIM != 0) {
-		std::cout << "The size of square_t isn't multiple of block size." << std::endl;
+		std::cout << "The capable size must be multiple of the block size." << std::endl;
 		throw std::exception();
 	}
-	if (APP_DIM != CAPABLE_DIM * 2) {
-		std::cout << "The size of square matrix must be twice CAPABLE_DIM." << std::endl;
+	if (APP_DIM % CAPABLE_DIM != 0) {
+		std::cout << "The total size must be multiple of the capable size." << std::endl;
 		throw std::exception();
 	}
 
 	try {
-		cuder<square_t> buf_1(make_cuder<square_t>());
-		cuder<square_t> buf_2(make_cuder<square_t>());
-		cuder<square_t> buf_3(make_cuder<square_t>());
-		cuder<square_t> buf_4(make_cuder<square_t>());
+		cuder<capable_t> buf_1(make_cuder<capable_t>());
+		cuder<capable_t> buf_2(make_cuder<capable_t>());
+		cuder<capable_t> buf_3(make_cuder<capable_t>());
+		cuder<capable_t> buf_4(make_cuder<capable_t>());
 
 		// Time Counter Initialization
 		cudaEvent_t start, stop;
 		cudaEventCreate(&start);
 		cudaEventCreate(&stop);
 
-		if (PRINT_TOTAL_TIME) {
-			cudaEventRecord(start, 0);
-		}
+		// Initialize
+		constexpr int pieceDim = APP_DIM / CAPABLE_DIM;
 
-		buf_1.load(init_A);
-		buf_2.load(init_identify);
-		square_inverse(buf_1, buf_2);
-		// buf_2 = A_inv
+		for (int row = 0; row < pieceDim; ++row) {
+			for (int col = 0; col < pieceDim; ++col) {
+				capable_t &buf = *buf_1;
 
-		buf_1.load(init_C);
-		square_mul(buf_1, buf_2, buf_3);
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
+				int glb_i = row * CAPABLE_DIM;
+				for (int i = 0; i < CAPABLE_DIM; ++i) {
 
-		buf_1.load(init_B);
-		square_mul(buf_3, buf_1, buf_4);
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-		// buf_4 = C * A_inv * B
-
-		buf_1.load(init_D);
-		square_add(buf_4, buf_1);
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-		// buf_4 = D - C * A_inv * B
-
-		buf_1.load(init_identify);
-		square_inverse(buf_4, buf_1);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-
-		square_mul(buf_1, buf_3, buf_4);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_3 = C * A_inv
-		// buf_4 = DL
-
-		buf_3.store("C_Ainv");
-		buf_4.store("DL");
-		// buf_1 = DR
-		// buf_2 = A_inv
+					int glb_j = col * CAPABLE_DIM;
+					for (int j = 0; j < CAPABLE_DIM; ++j) {
+						buf.data[i][j] = (glb_i <= glb_j) ? gf_int_t(glb_i + glb_j + 1) : gf_int_t(0);
+						++glb_j;
+					}
 		
-		buf_3.load(init_B);
-		square_mul(buf_2, buf_3, buf_4);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_4 = A_inv * B
+					++glb_i;
+				}
 
-		square_mul(buf_4, buf_1, buf_3);
-		// buf_1 = DR
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = A_inv * B
+				buf_1.store(piece(row, col));
+			}
 
-		buf_1.store("DR");
-		buf_4.load("C_Ainv");
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = C * A_inv
+			for (int col = 0; col < pieceDim; ++col) {
+				capable_t &buf = *buf_1;
 
-		square_mul(buf_3, buf_4, buf_1);
-		// buf_1 = UR * C * A_inv
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = C * A_inv
+				int glb_i = row * CAPABLE_DIM;
+				for (int i = 0; i < CAPABLE_DIM; ++i) {
 
-		square_add(buf_1, buf_2);
-		// buf_1 = UL
-		// buf_2 = A_inv
-		// buf_3 = UR
-		// buf_4 = C * A_inv
+					int glb_j = col * CAPABLE_DIM;
+					for (int j = 0; j < CAPABLE_DIM; ++j) {
+						buf.data[i][j] = (glb_i == glb_j) ? gf_int_t(1) : gf_int_t(0);
+						++glb_j;
+					}
+		
+					++glb_i;
+				}
 
-		buf_2.load("DL");
-		buf_4.load("DR");
-		// buf_1 = Up Left
-		// buf_2 = Down Left
-		// buf_3 = Up Right
-		// buf_4 = Down Right
-
-		if (PRINT_TOTAL_TIME) {
-			cudaEventRecord(stop, 0);
-			cudaEventSynchronize(stop);
-			float elapsedTime;
-			cudaEventElapsedTime(&elapsedTime, start, stop);
-			std::cout << "Total Time: " << elapsedTime / 1000 << " s." << std::endl;
+				buf_1.store(piece(row, pieceDim + col));
+			}
 		}
+
+		cudaEventRecord(start, 0);
+
+		// Calculate
+		for (int pivot_idx = 0; pivot_idx < pieceDim; ++pivot_idx) {
+			cudaEvent_t round_start, round_stop;
+			if (PRINT_VERBOSE) {
+				cudaEventCreate(&round_start);
+				cudaEventCreate(&round_stop);
+				cudaEventRecord(round_start, 0);
+			}
+
+			buf_2.load(piece(pivot_idx, pivot_idx));
+			buf_1.load(init_identify);
+			capable_inverse(buf_2, buf_1);
+			// buf_1 = inv(pivot_idx,pivot_idx)
+
+			for (int row = 0; row < pieceDim; ++row) {
+				if (row != pivot_idx) {
+					buf_2.load(piece(row, pivot_idx));
+					capable_mul(buf_2, buf_1, buf_3);
+					// buf_3 = coeff = (row, pivot) * (pivot, pivot)_inv
+	
+					for (int col = pivot_idx; col <= pivot_idx + pieceDim; ++col) {
+						buf_2.load(piece(pivot_idx, col));
+						capable_mul(buf_3, buf_2, buf_4);
+
+						buf_2.load(piece(row, col));
+						capable_add(buf_2, buf_4);
+						buf_2.store(piece(row, col));
+					}
+				}
+			}
+
+			for (int col = pivot_idx; col <= pivot_idx + pieceDim; ++col) {
+				if (col != pivot_idx) {
+					buf_2.load(piece(pivot_idx, col));
+					capable_mul(buf_1, buf_2, buf_3);
+					buf_3.store(piece(pivot_idx, col));
+				} else {
+					buf_3.load(init_identify);
+					buf_3.store(piece(pivot_idx, col));
+				}
+			}
+
+			if (PRINT_VERBOSE) {
+				cudaEventRecord(round_stop, 0);
+				cudaEventSynchronize(round_stop);
+
+				float elapsedTime;
+				cudaEventElapsedTime(&elapsedTime, round_start, round_stop);
+				std::cout << "Round " << pivot_idx << ": "
+					<< (elapsedTime/1000) << " s."
+					<< std::endl;
+			}
+		}
+		
+		cudaEventRecord(stop, 0);
+		cudaEventSynchronize(stop);
 
 		if (PRINT_RESULT) {
-			for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_1->data[i][j] << ' ';
+			for (int i = 0; i < pieceDim; ++i) {
+				std::ifstream ifs[pieceDim];
+				gf_int_t buf[CAPABLE_DIM];
+
+				for (int j = 0; j < pieceDim; ++j) {
+					ifs[j].open("tmp/" + piece(i, pieceDim+j) + ".bin", std::ios::binary);
+					if (!ifs[j])
+						throw bad_file_stream();
 				}
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_3->data[i][j] << ' ';
+
+				long long size = sizeof(gf_int_t) * CAPABLE_DIM / sizeof(char);
+				for (int row = 0; row < CAPABLE_DIM; ++row) {
+					for (int col = 0; col < pieceDim; ++col) {
+						ifs[col].read(reinterpret_cast<char *>(buf), size);
+						
+						for (int k = 0; k < CAPABLE_DIM; ++k) {
+							std::cout << std::hex << buf[k] << ' ';
+						}
+					}
+					std::cout << std::endl;
 				}
-				std::cout << std::endl;
-			}
-	
-			for (uint32_t i = 0; i < CAPABLE_DIM; ++i) {
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_2->data[i][j] << ' ';
+
+				for (int j = 0; j < pieceDim; ++j) {
+					ifs[j].close();
 				}
-				for (uint32_t j = 0; j < CAPABLE_DIM; ++j) {
-					std::cout << std::hex << buf_4->data[i][j] << ' ';
-				}
-				std::cout << std::endl;
 			}
 		}
-	
+
+		if (PRINT_TOTAL_TIME) {
+			float elapsedTime;
+			cudaEventElapsedTime(&elapsedTime, start, stop);
+			std::cout << "Total Time: " << (elapsedTime/1000) << " s." << std::endl;
+		}
+
 		// Time Counter Destruction
 		cudaEventDestroy(start);
 		cudaEventDestroy(stop);
